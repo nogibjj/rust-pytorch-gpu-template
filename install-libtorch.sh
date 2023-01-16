3abd7cbec4853f47356c63e58e5783b3436764ce
Noah Gift <noah.gift@gmail.com>
2023-01-16 15:15:49 +0000

    loading CUDA tools

---
+++ install-libtorch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#download libtorch v1.13.0 and extract and overwrite to libtorch with CUDA support 11.7
+rm -rf libtorch
+wget https://download.pytorch.org/libtorch/cu117/libtorch-cxx11-abi-shared-with-deps-1.13.1%2Bcu117.zip
+unzip libtorch-cxx11-abi-shared-with-deps-1.13.1+cu117.zip
