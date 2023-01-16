98925632a6bb3def691fedd32d39ad197db50c0a
Noah Gift <noah.gift@gmail.com>
2023-01-16 21:00:14 +0000

    adding examples

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source env.sh
 source /home/codespace/venv/bin/activate
 #append it to bash so every shell launches with it 
 echo 'source /home/codespace/venv/bin/activate' >> ~/.bashrc
